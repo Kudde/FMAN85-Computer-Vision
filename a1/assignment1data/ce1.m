f1150af2207dfd0ad2c8841299e6f067830d4a94
Ludde <ludde.llj@gmail.com>
2020-03-03 16:54:00 +0100

    a1 fix

--- a1/assignment1data/ce1.m
+++ a1/assignment1data/ce1.m
@@ -1,8 +1,12 @@
 D = load('compEx1.mat');
 x2d = D.x2D;
 x2d_flat = pflat(x2d);
-%plot(x2d_flat(1,:),x2d_flat(2,:),'.')
+figure
+plot(x2d_flat(1,:),x2d_flat(2,:),'.')
+axis equal;
 
 x3d = D.x3D;
 x3d_flat = pflat(x3d);
+figure
 plot3(x3d_flat(1,:),x3d_flat(2,:),x3d_flat(3,:),'.');
+axis equal;
